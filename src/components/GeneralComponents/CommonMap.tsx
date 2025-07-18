2c0d4cb3a50387d79639ab6a27c80bd17d0f325b
Сергеев Данил Алексеевич <@upr-asu-4.oblgas.nov>
2025-07-18 15:03:49 +0300

    add map

---
+++ src/components/GeneralComponents/CommonMap.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+const CommonMap: FC = () => {
+  return (
+    <section>
+      <iframe
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3Aa780f28655670fc8026de8085aa56f7d2b0e53241ad8802539fc861b5482b1ad&amp;source=constructor"
+        width="100%"
+        height="530"
+        title="Yandex map showing location"
+      ></iframe>
+    </section>
+  );
+};
+
+export default CommonMap;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import CommonLine from "../components/GeneralComponents/CommonLine";
+import CommonMap from "../components/GeneralComponents/CommonMap";
 import HomeAboutUsComponents from "../components/Home/HomeAboutUsComponents";
 import WelcomeHomeComponents from "../components/Home/WelcomeHomeComponents";
 
@@ -7,7 +8,8 @@ function HomePage() {
     <main>
         <WelcomeHomeComponents/>
         <HomeAboutUsComponents/>
-        <CommonLine title="Lorem ipsome" text="Lorem ipsum dolor sit amet consectetur adipisicing elit."/> 
+        <CommonMap/> 
+        <CommonLine title="Lorem ipsome" text="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
     </main>
   );
 }
